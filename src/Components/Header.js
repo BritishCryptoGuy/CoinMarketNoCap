c1228f5af283648d66be8fb8200d06db628b8c72
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-03-16 22:05:32 +0000

    Created Header, started home component

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import { NavLink, Router } from "react-router-dom";
+
+const headerStyle = {
+  headerDiv: {
+    height: "20%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    boxShadow: "0.5px 0.5px 0.5px 0.5px black",
+  },
+  h1Style: {
+    fontFamily:
+      "Inter,-apple-system,BlinkMacSystemFont,segoe ui,Roboto,Helvetica,Arial,sans-serif",
+  },
+  spanStyle: {
+    color: "red",
+  },
+};
+function Header() {
+  return (
+    <>
+      <div style={headerStyle.headerDiv}>
+        <h1 style={headerStyle.h1Style}>
+          CoinMarket<span style={headerStyle.spanStyle}>No</span>Cap
+        </h1>
+        <nav>
+          <NavLink path="/portfolio">Portfolio</NavLink>
+          <NavLink path="/watchlist">Watchlist</NavLink>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/Components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <div>Lots of crypto</div>
+    </>
+  );
+}
+
+export default Home;
