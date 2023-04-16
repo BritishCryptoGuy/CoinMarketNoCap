cea53716e41157513bafd9871d6867651f74717d
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-04-16 09:12:19 +0100

    Added light css to MarketAndVolume

--- src/Components/MarketAndVolume.js
+++ src/Components/MarketAndVolume.js
@@ -4,35 +4,55 @@ function MarketAndVolume(props) {
   return (
     <div id="marketCapDiv">
       <div className="marketCol">
-        <h3>Market Cap</h3>
-        <p>Rank: {currency.market_cap_rank}</p>
-        <p>${currency.market_cap.usd.toLocaleString()}</p>
-        <p>24 change: {currency.market_cap_change_percentage_24h}</p>
-        <p>
-          24h Volume / Market cap:{" "}
-          {currency.total_volume.usd / currency.market_cap.usd}
-        </p>
+        <h3 className="grey" style={{ paddingBottom: "10px" }}>
+          Market Cap
+        </h3>
+        <div>
+          <p className="grey">
+            Rank:{" "}
+            <span style={{ color: "black" }}>{currency.market_cap_rank}</span>
+          </p>
+          <p>${currency.market_cap.usd.toLocaleString()}</p>
+          <p>24 change: {currency.market_cap_change_percentage_24h}</p>
+          <p>
+            24h Volume / Market cap:{" "}
+            {currency.total_volume.usd / currency.market_cap.usd}
+          </p>
+        </div>
       </div>
       <div className="border"></div>
       <div className="marketCol">
-        <h3>Fully Diluted Market Cap</h3>
-        <p>
-          ${currency.fully_diluted_valuation.usd.toLocaleString() || "Unknown"}
-        </p>
+        <h3 className="grey" style={{ paddingBottom: "10px" }}>
+          Fully Diluted Market Cap
+        </h3>
+        <div>
+          <p>
+            $
+            {currency.fully_diluted_valuation.usd.toLocaleString() || "Unknown"}
+          </p>
+        </div>
       </div>
       <div className="border"></div>
 
       <div className="marketCol">
-        <h3>Volume</h3>
-        <p>24 Hour Volume: ${currency.total_volume.usd.toLocaleString()}</p>
+        <h3 className="grey" style={{ paddingBottom: "10px" }}>
+          Volume
+        </h3>
+        <div>
+          <p>24 Hour Volume: ${currency.total_volume.usd.toLocaleString()}</p>
+        </div>
       </div>
       <div className="border"></div>
 
       <div className="marketCol">
-        <h3>Circulating Supply</h3>
-        <p>{currency.circulating_supply}</p>
-        <p>Max Supply: {currency.max_supply || "--"}</p>
-        <p>Total Supply: {currency.total_supply || "--"}</p>
+        <h3 className="grey" style={{ paddingBottom: "10px" }}>
+          Circulating Supply
+        </h3>
+        <div>
+          <p>{currency.circulating_supply.toLocaleString()}</p>
+          <p>Max Supply: {currency.max_supply?.toLocaleString() || "--"}</p>
+          <p>Total Supply: {currency.total_supply.toLocaleString() || "--"}</p>
+        </div>
       </div>
     </div>
   );
