4f6b3af3bda0f663cf6f2011c9f9579694f8d691
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-04-30 12:16:21 +0100

    Added NPM package html-react-parser in order to filter out link elements for description

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -1,13 +1,34 @@
+import parse from "html-react-parser";
+
 function Description(props) {
   let currency = props.prop;
   let description = currency.description?.en || "No description available";
+
+  function descriptionFilter(des) {
+    const apiResponse = des;
+    const filteredDescription = parse(apiResponse, {
+      replace: ({ attribs, children }) => {
+        if (attribs && attribs.href) {
+          return (
+            <a href={attribs.href} target="_blank">
+              {children[0].data}
+            </a>
+          );
+        }
+      },
+    });
+
+    return filteredDescription;
+  }
 
   return (
     <div id="descriptionDiv">
       <h4 style={{ padding: "20px", textAlign: "center" }}>
         What is {currency.name}?
       </h4>
-      <div>{description}</div>
+      <div id="descriptionData">
+        {description && descriptionFilter(description)}
+      </div>
     </div>
   );
 }
