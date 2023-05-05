90857f2e1eeca5c2d6535fdaebe632ed40e38946
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-05-05 05:53:31 +0100

    Added fontAwesome packages

--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 function Socials(prop) {
+  const element = <FontAwesomeIcon icon={faEnvelope} />;
+
   return (
     <div>
-      <div></div>
+      <div>{element} </div>
     </div>
   );
 }
