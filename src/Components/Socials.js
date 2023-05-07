d7d6b6d8a30910a98eaec09f0fd7ee837fa7d813
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-05-07 07:33:47 +0100

    Added base socials logo's. Need to add links individually for each crypto where available

--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -1,12 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faFacebook,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Socials(prop) {
-  const element = <FontAwesomeIcon icon={faEnvelope} />;
+  const socialsStyle = {
+    socialsDiv: {
+      display: "flex",
+      justifyContent: "center",
+    },
+    logo: {
+      fontSize: "40px",
+      padding: "20px",
+    },
+  };
+  const twitterLogo = (
+    <FontAwesomeIcon icon={faTwitter} style={{ color: "#1da1f2" }} />
+  );
+  const facebookLogo = (
+    <FontAwesomeIcon icon={faFacebook} style={{ color: "#3b5998" }} />
+  );
+  const githubLogo = (
+    <FontAwesomeIcon icon={faGithub} style={{ color: "#000000" }} />
+  );
 
   return (
-    <div>
-      <div>{element} </div>
+    <div style={socialsStyle.socialsDiv}>
+      <div style={socialsStyle.logo}>{twitterLogo} </div>
+      <div style={socialsStyle.logo}>{facebookLogo}</div>
+      <div style={socialsStyle.logo}>{githubLogo}</div>
     </div>
   );
 }
