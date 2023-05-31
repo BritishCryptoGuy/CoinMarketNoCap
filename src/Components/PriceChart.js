d510666bf5eb76e734ac1b549f443391406f79e6
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-05-31 15:56:57 +0100

    Removed fetchChart function from useEffect on priceChart. Added options to change dataRange for crypto price chart

--- src/Components/PriceChart.js
+++ src/Components/PriceChart.js
@@ -23,31 +23,55 @@ ChartJS.register(
   Legend
 );
 
+const priceChartStyle = {
+  button: {
+    border: "2px solid #8c1c13",
+    padding: "2px 6px",
+    borderRadius: "8px",
+  },
+};
+
 function PriceChart(prop) {
   let cryptoName = prop.prop.id;
   let [cryptoObj, setCryptoObj] = useState(false);
 
+  function fetchChart(days) {
+    console.log(days);
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${cryptoName}/market_chart?vs_currency=usd&days=${
+        days || "7"
+      }`,
+      {
+        headers: {
+          accept: "application/json",
+        },
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setCryptoObj(data.prices));
+  }
+
   useEffect(() => {
-    function fetchChart() {
-      fetch(
-        `https://api.coingecko.com/api/v3/coins/${cryptoName}/market_chart?vs_currency=usd&days=3d`,
-        {
-          headers: {
-            accept: "application/json",
-          },
-          method: "GET",
-        }
-      )
-        .then((response) => response.json())
-        .then((data) => setCryptoObj(data.prices));
-    }
     fetchChart();
   }, [setCryptoObj]);
+
+  function changeDataRange(req) {
+    console.log(req);
+    if (req === "Max") {
+      fetchChart("max");
+    } else if (req === "24h") {
+      fetchChart("1");
+    } else if (req === "1y") {
+      fetchChart("365");
+    } else {
+      fetchChart(req.slice(0, -1));
+    }
+  }
 
   let chartData = !cryptoObj
     ? false
     : cryptoObj.map((val) => ({ x: val[0], y: val[1].toFixed(2) }));
-  chartData && console.log(chartData);
 
   const options = {
     responsive: true,
@@ -68,6 +92,19 @@ function PriceChart(prop) {
   return (
     <div style={{ width: "70%" }}>
       <Line options={options} data={data} />
+      <div
+        style={{ display: "flex", justifyContent: "space-evenly" }}
+        onClick={(e) => changeDataRange(e.target.innerHTML)}
+      >
+        <p style={priceChartStyle.button}>24h</p>
+        <p style={priceChartStyle.button}>7d</p>
+        <p style={priceChartStyle.button}>14d</p>
+        <p style={priceChartStyle.button}>30d</p>
+        <p style={priceChartStyle.button}>90d</p>
+        <p style={priceChartStyle.button}>180d</p>
+        <p style={priceChartStyle.button}>1y</p>
+        <p style={priceChartStyle.button}>Max</p>
+      </div>
     </div>
   );
 }
